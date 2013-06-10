acfaf142e2d93ca3aa4443f4878eb6bc3efedf90
Eric Robitaille <eric@vianet.ca>
2013-06-10 02:46:13 -0400

    added fake test harness

---
+++ test/test.js
@@ -0,0 +1,17 @@
+// note: test harness will not run until resource is actually a module and included in devDependencies
+/*
+var resource = require('resource'),
+	upload = resource.use('upload');
+
+resource.use('http');
+
+// Testing init method on resource
+resource.http.listen({}, function(){
+	upload.init({
+			app: resource.http.app
+		}, function(err, test){
+
+			console.log(err);
+	});
+});
+*/
